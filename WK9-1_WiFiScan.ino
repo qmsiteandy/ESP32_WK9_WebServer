553adacf602756c30e1229e4e5ee23beb9bac64d
qmsiteandy <ht93084@gmail.com>
2021-11-13 00:49:35 +0800

    Add files via upload

---
+++ WK9-1_WiFiScan.ino
@@ -0,0 +1,27 @@
+#include "WiFi.h"
+
+void setup(){
+    Serial.begin(115200);
+}
+
+void loop(){
+    Serial.println("scan start");
+    int wifiCount = WiFi.scanNetworks();  //掃描網路，並將掃描到的網路數量存入
+    Serial.println("scan done");
+    if (wifiCount == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(wifiCount);
+        Serial.println(" networks found");
+        for (int i = 0; i < wifiCount; ++i) {
+            //顯示無線網路SSID, RSSI, 加密
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.println(WiFi.SSID(i));
+            delay(10);
+        }
+    }
+    Serial.println("");
+    //等候五秒後，重新掃描
+    delay(5000);
+}
